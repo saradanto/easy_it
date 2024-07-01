3e820c6b30445aa717dda3d6c76236322d0c9301
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-01 21:01:19 +0200

    :init project :install bootstrap :create component layout

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <button class="btn btn-outline-dark nav-link" href="#">login</button>
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-outline-dark nav-link" href="#">register</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
