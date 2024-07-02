338bd5b921529dcab22aa493c1d1f8bf0439d615
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-02 18:51:42 +0200

    :creating migration category :create db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    public $categories = [
+        'tecnologia',
+        'fotografia',
+        'telefonia',
+        'arredamento',
+        'elettrodomestici',
+        'sport',
+        'animali domestici',
+        'abbigliamento',
+        'motori'
+    ];
+
     public function run(): void
     {
+        foreach ($this->categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
+        
         // User::factory(10)->create();
 
         User::factory()->create([
