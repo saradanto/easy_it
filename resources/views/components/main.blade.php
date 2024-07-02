72e915cd32c0f3ebe8f9ffce952ea57220042bb3
Daniele Cordova <daddo.co.2000@gmail.com>
2024-07-02 19:27:36 +0200

    add navbar + main + ourstyle.css + routes

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Easy.it</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    {{-- PERSONAL CSS --}}
+    <link rel="stylesheet" href="css/ourstyle.css">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
