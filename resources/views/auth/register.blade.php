a6d97082cae2fa4d4c47cfa99b9bce22d38cb340
gaspare d'antone <dantogas@gmail.com>
2024-07-03 18:38:06 +0200

    add form fortify

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,17 +5,17 @@
 <section class="vh-100 bg-image"
   style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="container h-100 ">
+      <div class="row d-flex justify-content-center mt-2 h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+              <h2 class="text-uppercase text-center mb-2">Crea il tuo account</h2>
 
               <form class="" action="{{ route('register')}}" method="POST">
                 @csrf
 
-                <div data-mdb-input-init class="form-outline mb-4">
+                <div data-mdb-input-init class="form-outline mb-2">
                   <input type="text" id="name" value="{{old('name')}}" name="name" class="form-control form-control-lg" />
                   <label class="form-label" for="name">Nome</label>
                   @error('name')
@@ -23,7 +23,7 @@
                       
                   @enderror
                 </div>
-                <div data-mdb-input-init class="form-outline mb-4">
+                <div data-mdb-input-init class="form-outline mb-2">
                     <input type="email" id="email" value="{{old('email')}}" name="email" class="form-control form-control-lg" />
                     <label class="form-label" for="name">Email</label>
                     @error('email')
@@ -32,12 +32,12 @@
                     @enderror
                   </div>
 
-                <div data-mdb-input-init class="form-outline mb-4">
+                <div data-mdb-input-init class="form-outline mb-2">
                   <input type="password" id="password" name="password" class="form-control form-control-lg" />
                   <label class="form-label" for="password">Password</label>
                 </div>
 
-                <div data-mdb-input-init class="form-outline mb-4">
+                <div data-mdb-input-init class="form-outline mb-2">
                   <input type="password" id="password_confirmation" name="password_confirmation" class="form-control form-control-lg" />
                   <label class="form-label" for="password_confirmation">Conferma la tua password</label>
                 </div>
