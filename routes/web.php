2b6da53a28720dc57a7285c054950caaa261a70f
juriboragine <juri25@outlook.it>
2024-07-03 19:25:31 +0200

    situazioni

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController; 
-use App\Http\Controllers\ArticleController;
+use app\Http\Controllers\ArticleController;
 
 Route::resource('article', ArticleController::class);
 Route::get('/', [PageController::class, 'homepage'])->name('homepage');
 Route::get('/notice', [PageController::class, 'notice'])->name('notice');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-
