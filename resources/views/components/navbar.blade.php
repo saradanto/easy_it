721e1d7e04447bd9e8109ff1be057a023c0d1828
Daniele Cordova <daddo.co.2000@gmail.com>
2024-07-03 17:30:52 +0200

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,78 +5,42 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('homepage') }}">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('notice') }}">Annunci</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('contact') }}">Contatti</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav ms-auto">
-        
-        <div class="block-header block-account turan-dropdown open">
-          <a class="woo-user-link" href="https://turan.kutethemes.net/my-account/" data-turan="turan-dropdown">
-            <span class="icon"><i class="turanicon-user"></i></span>
-            <span class="text">Login/Register</span>
-          </a>
-          <div class="header-account turan-submenu sub-menu">
-            <div class="woocommerce"><div class="woocommerce-notices-wrapper"></div>
-            <div class="u-columns col2-set" id="customer_login">
-              <div class="u-column1 col-1">
-                <h2>Login</h2>
-                <form class="woocommerce-form woocommerce-form-login login" method="post">
-                  <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="username">Username or email address&nbsp;<span class="required">*</span></label>
-                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="">			
-                  </p>
-                  <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="password">Password&nbsp;<span class="required">*</span></label>
-                    <span class="password-input"><input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password"><span class="show-password-input"></span></span>
-                  </p>
-                  <p class="form-row">
-                    <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
-                      <input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever"> <span>Remember me</span>
-                    </label>
-                    <input type="hidden" id="woocommerce-login-nonce" name="woocommerce-login-nonce" value="7315485e96"><input type="hidden" name="_wp_http_referer" value="/home-21/">				<button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="Log in">Log in</button>
-                  </p>
-                  <p class="woocommerce-LostPassword lost_password">
-                    <a href="https://turan.kutethemes.net/my-account/lost-password/">Lost your password?</a>
-                  </p>
-                </form>
-              </div>
-              <div class="u-column2 col-2">
-                <h2>Register</h2>
-                <form method="post" class="woocommerce-form woocommerce-form-register register">
-                  <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="reg_email">Email address&nbsp;<span class="required">*</span></label>
-                    <input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value="">			
-                  </p>
-                  <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="reg_password">Password&nbsp;<span class="required">*</span></label>
-                    <span class="password-input"><input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" autocomplete="new-password"><span class="show-password-input"></span></span>
-                  </p>
-                  <div class="woocommerce-privacy-policy-text"><p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our <a href="https://turan.kutethemes.net/?page_id=3" class="woocommerce-privacy-policy-link" target="_blank">privacy policy</a>.</p>
-                  </div>
-                  <p class="woocommerce-form-row form-row">
-                    <input type="hidden" id="woocommerce-register-nonce" name="woocommerce-register-nonce" value="7a52c78a5f"><input type="hidden" name="_wp_http_referer" value="/home-21/">				<button type="submit" class="woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="Register">Register</button>
-                  </p>
-                </form>
-              </div>
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="{{ route('homepage') }}">Home</a>
+            <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <svg class="svg-inline--fa fa-bars" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"></path></svg><!-- <i class="fas fa-bars"></i> Font Awesome fontawesome.com -->
+            </button>
+            <div class="navbar-collapse collapse" id="navbarResponsive" style="">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#about">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#projects">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#signup">Log Out</a></li>
+                </ul>
             </div>
-          </div>            
         </div>
-      </div>
+    </nav>
+      {{-- <div class="container ">
+        <ul>
+          <li>
+            <a href="#" class="ourList btn btn-info btn-lg mx-2">
+              <span class="glyphicon glyphicon-log-out logOut"></span> Log in
+            </a>
+          </li>
+          <li>
+            <a href="#" class="ourList btn btn-info btn-lg mx-2">
+              <span class="glyphicon glyphicon-log-out logOut"></span> Register
+            </a>
+          </li>
+          <li>
+            <a href="#" class="ourList btn btn-info btn-lg mx-2">
+              <span class="glyphicon glyphicon-log-out logOut"></span> Log out
+            </a>
+          </li>
+        </ul>
+      </div> --}}
       
-      <li>
-        <a href="#" class="btn btn-info btn-lg mx-2">
-          <span class="glyphicon glyphicon-log-out logOut"></span> Log out
-        </a>
-      </li>
     </ul>
   </div>
-</div>
 </nav>
