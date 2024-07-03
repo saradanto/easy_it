3baa7d4d96c9b271a93ceb0fc1f6dcc81509b405
juriboragine <juri25@outlook.it>
2024-07-03 13:47:00 +0200

    Pagecontroller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,7 +8,7 @@ class PageController extends Controller
 {
    public function homepage()
   {
-     $notices = Notice::take(6)->orderBy('created_at', 'desc')->get();
+     
      return view('homepage');
 
 
@@ -18,7 +18,7 @@ class PageController extends Controller
   public function notice ()
 
   {
-     $notices = Notice::take(6)->orderBy('created_at','desc')->paginate(6);
+     
      return view ('notice');
 
   }
