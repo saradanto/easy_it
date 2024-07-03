a3957206443e09207f427e5b028d236fc1727446
gaspare d'antone <dantogas@gmail.com>
2024-07-03 18:52:07 +0200

    remove pac

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController; 
-use App\Http\Controllers\ArticleController;
+use app\Http\Controllers\ArticleController;
 
 Route::get('/', [PageController::class, 'homepage'])->name('homepage');
 Route::get('/notice', [PageController::class, 'notice'])->name('notice');
