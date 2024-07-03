15a8fe557095e2f276d593bc688fb5ff70a4e9e9
gaspare d'antone <dantogas@gmail.com>
2024-07-03 19:22:24 +0200

    add a

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController; 
-use app\Http\Controllers\ArticleController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', [PageController::class, 'homepage'])->name('homepage');
 Route::get('/notice', [PageController::class, 'notice'])->name('notice');
