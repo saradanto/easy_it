768b22bdc6f6125dcf92c0d7a080f75e29ec8895
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-04 21:42:09 +0200

    :edit homepage, add article with orderBy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,10 +5,10 @@
         <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-lg-8 col-xl-7 col-xxl-6">
                 <div class="my-5 text-center text-xl-start">
-                    <h1 class="display-5 fw-bolder mb-2">A Bootstrap 5 template for modern businesses</h1>
-                    <p class="lead fw-normal text-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                    <h1 class="display-5 fw-bolder mb-2">Aquista e vendi i tuoi articoli preferiti in modo Easy.it</h1>
+                    <p class="lead fw-light text-50 mb-4">Qui puoi inserire e gestire i tuoi annunci!</p>
                     <div class="d-grid gap-1 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                      <a href="{{route('article.create')}}" class="btn btn-outline-primary btn-lg me-md-4 rounded-5"><i class="bi bi-plus-circle me-2"></i>Inserisci un articolo</a>
+                      <a href="{{route('article.create')}}" class="btn btn-outline-primary btn-lg me-md-4 rounded-5 px-4">Inserisci un articolo</a>
                       <a class="btn btn-outline-dark btn-lg px-4 rounded-5" href="{{route('notice')}}">Cerca annuncio</a>
                     </div>
                 </div>
@@ -18,9 +18,9 @@
     </div>
 </header>
 
-<h2 class="h2 fw-bold text-center" style="margin: 150px 0px">Ultimi annunci</h2>
+<h2 class="h2 fw-bold text-center" style="margin: 200px 0px 50px 0px;">Ultimi annunci</h2>
 
-<div class="container mt-5 d-flex flex-wrap">
+<div class="container mt-5 d-flex flex-wrap justify-content-center">
 
   {{-- @for ($i = 0; $i < 4; $i++)
     @if (count($articles) > $i)
@@ -39,42 +39,21 @@
   @endfor --}}
 
 
-
-      <div class="card rounded-5 border-0 mx-3" style="width: 18rem; height: auto;">
-        <img src="https://picsum.photos/200/200?random=1" class="card-img-top img-fluid rounded-4" alt="...">
-        <div class="card-body">
-          <p class="card-text fw-bold">Categoria</p>
-          <h5 class="card-title fw-bold">Titolo</h5>
-          <p class="card-text">100 €</p>
-        </div>
-      </div>
-
-      <div class="card rounded-5 border-0 mx-3" style="width: 18rem; height: auto;">
-        <img src="https://picsum.photos/200/200?random=1" class="card-img-top img-fluid rounded-4" alt="...">
-        <div class="card-body">
-          <p class="card-text fw-bold">Categoria</p>
-          <h5 class="card-title fw-bold">Titolo</h5>
-          <p class="card-text">100 €</p>
-        </div>
-      </div>
-
-      <div class="card rounded-5 border-0 mx-3" style="width: 18rem; height: auto;">
-        <img src="https://picsum.photos/200/200?random=1" class="card-img-top img-fluid rounded-4" alt="...">
-        <div class="card-body">
-          <p class="card-text fw-bold">Categoria</p>
-          <h5 class="card-title fw-bold">Titolo</h5>
-          <p class="card-text">100 €</p>
-        </div>
-      </div>
-
-      <div class="card rounded-5 border-0 mx-3" style="width: 18rem; height: auto;">
-        <img src="https://picsum.photos/200/200?random=1" class="card-img-top img-fluid rounded-4" alt="...">
-        <div class="card-body">
-          <p class="card-text fw-bold">Categoria</p>
-          <h5 class="card-title fw-bold">Titolo</h5>
-          <p class="card-text">100 €</p>
-        </div>
-      </div>
+@forelse ($articles as $article)
+<div class="card rounded-5 border-0 mx-5 my-5" style="width: 20rem; height: auto;">
+  <img src="https://picsum.photos/200/200?random=1" class="card-img-top img-fluid rounded-4" alt="...">
+  <div class="card-body">
+    <p class="card-text fw-bold">{{$article->category->name}}</p>
+    <h5 class="card-title fw-bold">{{$article->title}}</h5>
+    <p class="card-text">{{$article->price}}</p>
+  </div>
+</div>
+@empty
+<div class="card rounded-5 border-0 mx-3" style="width: 18rem; height: auto;">
+  nessun articolo
+</div>
+@endforelse
+      
 
 </div>
 
