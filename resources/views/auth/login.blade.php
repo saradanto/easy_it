fb121564ffd64ea881b5a4093ba555d3ccb2da0c
juriboragine <juri25@outlook.it>
2024-07-04 20:39:41 +0200

    background color changed form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,10 +47,7 @@
       </div>
     </div> --}}
   </section>
-
-  <section class="vh-100 bg-image"
-  style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
-  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+  <header class="" style="background-color: #CBE5EE; padding: 130px 0px;">
     <div class="container h-100 ">
       <div class="row d-flex justify-content-center mt-2 h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
