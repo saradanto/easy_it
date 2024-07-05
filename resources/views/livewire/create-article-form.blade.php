dd5f6afefdd19689fb90647a81017a1e4898bdc2
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-05 21:27:40 +0200

    edit navbar, edit homepage, edit graphic create, add link footer

--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -13,35 +13,35 @@
 </header>
 
 <div class="container my-5 py-5 d-flex justify-content-center">
-<div class="col-12 border p-5 rounded" style="width: 800px"> 
+<div class="col-12 border p-5 rounded-5" style="width: 800px"> 
     <a class="btn btn-outline-primary rounded-5 mb-5" href="{{route('article.index')}}"><i class="bi bi-arrow-left-short me-2"></i>Torna indietro</a>
     @if (session()->has('status'))
         {{ session('status') }}
     @endif
     <form wire:submit="store">
         <div class="form-floating mb-5">
-            <input class="form-control" wire:model.blur="title" type="text" id="title">
+            <input class="form-control border-0 border-bottom" wire:model.blur="title" type="text" id="title">
             <label for="title">Titolo</label>
             @error('title')
                 {{ $message }}
             @enderror
         </div>
         <div class="form-floating mb-5">
-            <input class="form-control" wire:model.blur="description" type="text">
+            <input class="form-control border-0 border-bottom" wire:model.blur="description" type="text">
             <label for="description" id="description">Descrizione</label>
             @error('description')
                 {{ $message }}
             @enderror
         </div>
         <div class="form-floating mb-5">
-            <input class="form-control" wire:model.blur="price" id="price"></input>
+            <input class="form-control border-0 border-bottom" wire:model.blur="price" id="price"></input>
             <label for="price">Price</label>
             @error('price')
                 {{ $message }}
             @enderror
         </div>
         <div class=" mb-5">
-            <select wire:model.blur="category_id" class="form-select" aria-label="Default select example" id="category">
+            <select wire:model.blur="category_id" class="form-select rounded-5" aria-label="Default select example" id="category">
                 <option value="" selected>Seleziona una categoria</option>
                 @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
@@ -54,7 +54,7 @@
             @enderror
         </div>
         <div class="d-grid">
-            <button class="btn btn-outline-primary rounded-5 btn-lg" type="submit">Salva</button>
+            <button class="btn btn-outline-primary rounded-5 mb-5" type="submit">Salva</button>
         </div>
     </form>
 </div>
