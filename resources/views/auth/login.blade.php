78c939df3e97a683f1e596e003640521e8974b3c
gaspare d'antone <dantogas@gmail.com>
2024-07-06 14:22:58 +0200

    start sociallite e done hide/showpassword

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-main>
-  <section class="vh-100 "
+  <section class="vh-100% "
   style="background-color: #CBE5EE; padding: 130px 0px;">
   <div class="mask d-flex align-items-center h-100 ">
     <div class="container h-100 ">
@@ -7,28 +7,41 @@
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-               <h2 class="text-uppercase text-center mb-2">Login</h2>
+               <h2 class="text-uppercase text-center mb-2 text-info ">Login</h2>
 
 
                 <form action="{{ route('login')}}" method="POST">
                    @csrf
 
                   <div data-mdb-input-init class="form-outline    mb-4">
-                    <label for="email">Inserisci la tua email</label>
-                    <input  type="email" class="form-control border-0 border-bottom" id="email" name="email" placeholder="name@example.com" required>
+                    {{-- <label for="email">Inserisci la tua email</label> --}}
+                    <input  type="email" class="form-control border-0 rounded-0 border-bottom" id="email" name="email" placeholder=" &#x1F464;name@example.com" required>
                   </div>  
                 
                  
 
                  <div data-mdb-input-init class="form-outline mb-4">
-                    <label for="password">Password</label>
-                   <input type="password" class="form-control" name="password" id="Password" placeholder="Password" required>
+                    {{-- <label for="password">Password</label> --}}
+                   <input type="password" class="form-control border-0 rounded-0 border-bottom mx-1  " name="password" id="passwordField" placeholder="&#x1F512;password" required><i class="bi bi-eye-slash" id="togglePassword"></i>
+
                   </div>  
                 
+                 <div class="text-center">
+
+                   <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-info me-md-4 rounded-4" type="submit">Login</button>
+                 </div>
                  
-                 <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary me-md-4 rounded-4" type="submit">Login</button>
-                  <a href="{{ route('register') }}" class="btn  text-dark">Non sei registrato?Clicca qui</a>
                 </form>
+                
+                  <p class="text-center text-muted mt-3 mb-0">Non sei registrato?  <a href="{{ route('register')}}"
+                    class="fw-bold text-body"><u>Clicca qui</u></a></p>
+                    <p class="text-center">
+                      o esegui l'accesso con
+                    </p>
+                    <div class="text-center">
+                      <button class="btn btn-outline-danger rounded-4 me-md-4">google</button>
+                    </div>
+
               </div>
             
             </div>
@@ -38,6 +51,11 @@
     </div>
 
   </section>
+
+
+
+
+  
 
 
 </x-main> 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,22 +2,22 @@
 
 <x-main>
 
-<section class="vh-100 "
+<section class="vh-100% "
   style="background-color: #CBE5EE; padding: 130px 0px;">
   <div class="mask d-flex align-items-center h-100 ">
     <div class="container h-100 ">
-      <div class="row d-flex justify-content-center mt-2 h-100">
+      <div class="row d-flex justify-content-center  h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-2">Crea il tuo account</h2>
+              <h2 class="text-uppercase text-center mb-2 text-info">Crea il tuo account</h2>
 
               <form class="" action="{{ route('register')}}" method="POST">
                 @csrf
 
-                <div data-mdb-input-init class="form-outline mb-2">
-                  <label class="form-label" for="name">Nome</label>
-                  <input type="text" id="name" value="{{old('name')}}" name="name" class="form-control form-control-lg" />
+                <div data-mdb-input-init class="form-outline mb-2 ">
+              
+                  <input type="text" id="name" value="{{old('name')}}" name="name" class="form-control form-control-lg border-0 rounded-0 border-bottom " placeholder="&#x1F464;nome" />
                   
                   @error('name')
                     <span>{{ $message}}</span>
@@ -25,8 +25,9 @@
                   @enderror
                 </div>
                 <div data-mdb-input-init class="form-outline mb-2">
-                     <label class="form-label" for="name">Email</label>
-                    <input type="email" id="email" value="{{old('email')}}" name="email" class="form-control form-control-lg" />
+                  
+                    
+                    <input type="email" id="email" value="{{old('email')}}" name="email" class="form-control form-control-lg border-0 rounded-0 border-bottom" placeholder="&#x1F4E7;email " />
                     
                     @error('email')
                       <span>{{ $message}}</span>
@@ -34,32 +35,36 @@
                     @enderror
                   </div>
 
-                <div data-mdb-input-init class="form-outline mb-2">
-                  <label class="form-label" for="password">Password</label>
-                  <input type="password" id="password" name="password" class="form-control form-control-lg" />
+                <div data-mdb-input-init class="form-outline mb-2 ">
+                  
+                  <input type=" password " id="password" name="password" class="form-control form-control-lg border-0 rounded-0 border-bottom" placeholder="&#x1F512;password " >
+
                   
                 </div>
 
                 <div data-mdb-input-init class="form-outline mb-2">
-                  <label class="form-label" for="password_confirmation">Conferma la tua password</label>
-                  <input type="password" id="password_confirmation" name="password_confirmation" class="form-control form-control-lg" />
+                  
+                    <input type="password" id="password" name="password_confirmation" class="form-control form-control-lg border-0 rounded-0 border-bottom"  placeholder="&#x1F512;conferma password " />
+                  
+               
                   
                 </div>
 
-                {{-- <div class="form-check d-flex justify-content-center mb-5">
-                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
-                  <label class="form-check-label" for="form2Example3g">
-                    I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
-                  </label>
-                </div> --}}
+            
 
                 <div class="d-flex justify-content-center">
                   <button  type="submit" data-mdb-button-init
-                    data-mdb-ripple-init class="btn btn-outline-primary me-md-4 rounded-4">Registrati</button>
+                    data-mdb-ripple-init class="btn btn-outline-info me-md-4 rounded-4">Registrati</button>
                 </div>
 
-                <p class="text-center text-muted mt-5 mb-0">Hai gia un account?  <a href="{{ route('login')}}"
+                <p class="text-center text-muted mt-3 mb-0">Hai gia un account?  <a href="{{ route('login')}}"
                     class="fw-bold text-body"><u>Login here</u></a></p>
+                    {{-- <p class="text-center">
+                      o esegui l'accesso con
+                    </p>
+                    <div class="text-center">
+                      <button class="btn btn-outline-info rounded-4 me-md-4">google</button>
+                    </div> --}}
 
               </form>
 
@@ -70,5 +75,11 @@
     </div>
   </div>
 </section>
+
+
+<input type="password" id="passwordField" placeholder="Inserisci la tua password">
+<i class="bi bi-eye-slash" id="togglePassword"></i>
+
+
 
 </x-main>
