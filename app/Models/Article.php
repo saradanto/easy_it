69a842a541f06e02aa783c52143e5904433f5c75
Sara D <sara.dantonio@live.it>
2024-07-08 20:52:07 +0200

    added setAccepted and toBeRevisedCount methods in Article Model, added 'is_accepted' in filtroscout render, accept and reject methods in RevisorController

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -33,4 +33,14 @@ class Article extends Model
             'category' => $this->category
         ];
     }
+
+    public function setAccepted($value){
+        $this->is_accepted = $value;
+        $this->save();
+        return true;
+    }
+
+    public static function toBeRevisedCount(){
+        return Article::where('is_accepted', null)->count();
+    }
 }
