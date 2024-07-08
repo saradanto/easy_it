08d365b79426c3ee2ae33cecc1d3f42055884905
Daniele Cordova <daddo.co.2000@gmail.com>
2024-07-08 17:29:05 +0200

    css

--- resources/views/daniele.blade.php
+++ resources/views/daniele.blade.php
@@ -5,25 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    {{-- SHOW --}}
     <nav>
         <input type="checkbox" id="check">
-        <label for="check">
+        <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
         </label>
         <label class="logo">Easy.it</label>
-        <ul>
+        <ul class="ourList">
             <li>
                 <a href="{{route('homepage')}}" class="active">Home</a>
             </li>
             <li>
                 <a href="{{route('juri')}}">About</a>
             </li>
-            <li>
-                <a href="{{route('byCategory')}}">Services</a>
-            </li>
+            {{-- <li>
+                <a href="{{route('byCategory, ['category'=>$byCategory]')}}">Services</a>
+            </li> --}}
             <li>
                 <a href="{{route('contact')}}">Contact</a>
             </li>
