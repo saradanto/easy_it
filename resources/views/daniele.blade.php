252495ec617874399039b2def0550a0e69d1e6ac
Daniele Cordova <daddo.co.2000@gmail.com>
2024-07-08 18:49:20 +0200

    swiper

--- resources/views/daniele.blade.php
+++ resources/views/daniele.blade.php
@@ -1,35 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<x-main>
+    <body>
+        <div class="container">
+            <main>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                    </div>
+                    <div class="col-12 col-md-6">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    {{-- SHOW --}}
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">Easy.it</label>
-        <ul class="ourList">
-            <li>
-                <a href="{{route('homepage')}}" class="active">Home</a>
-            </li>
-            <li>
-                <a href="{{route('juri')}}">About</a>
-            </li>
-            {{-- <li>
-                <a href="{{route('byCategory, ['category'=>$byCategory]')}}">Services</a>
-            </li> --}}
-            <li>
-                <a href="{{route('contact')}}">Contact</a>
-            </li>
-        </ul>
-    </nav>
-</body>
-</html>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </body>
+</x-main>
