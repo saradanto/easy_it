7d815c020116d1711797134584dbdb7732b7da7f
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-09 19:12:02 +0200

    fix swiper

--- resources/views/daniele.blade.php
+++ resources/views/daniele.blade.php
@@ -2,57 +2,54 @@
     <body>
         {{-- container-->row-->col12/colmd6*2 --}}
         {{-- container-->row-->col12/colmd3*4 --}}
-        <div class="container container-fluid">
+        <div class="container container-fluid mt-5 pt-5">
             <div class="row">
+                <div class="col-12 col-md-6">
                 <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2"> {{-- swiper-navigation swiper-pagination --}}
                     <div class="swiper-wrapper">
-                        <div class="col-12">
-                            <div class="col-12 col-md-6 swiper-slide">
+                            <div class="swiper-slide">
                                 <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
                             </div>
-                            <div class="col-12 col-md-6 swiper-slide">
-                                <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                            <div class="swiper-slide">
+                                <img src="https://dummyimage.com/300x300/a3a3a3/fff" alt="">
                             </div>
-                            <div class="col-12 col-md-6 swiper-slide">
-                                <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                            <div class="swiper-slide">
+                                <img src="https://dummyimage.com/300x300/b5b5b5/fff" alt="">
                             </div>
-                            <div class="col-12 col-md-6 swiper-slide">
-                                <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                            <div class="swiper-slide">
+                                <img src="https://dummyimage.com/300x300/c6c6c6/fff" alt="">
                             </div>
-                            <div class="col-12 col-md-6 swiper-slide">
-                                <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                            <div class="swiper-slide">
+                                <img src="https://dummyimage.com/300x300/a1a1a1/fff" alt="">
                             </div>
-                            <div class="col-12 col-md-6 swiper-slide">
-                                <h4 class="txt-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                                {{-- stars --}}
-                                {{-- price --}}
-                                {{-- description articolo --}}
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, eos laboriosam! Aliquam, esse nihil adipisci quas quisquam earum hic modi consequatur quibusdam omnis quod est sint veniam corrupti velit! Non.</p>
-                                {{-- button(quantità + aggiungi al carrello) --}}
-                                {{-- preferiti --}}
-                                {{-- description categorie + colori etc... --}}
-                            </div>
-                        </div>
                     </div>
                     <div class="swiper-button-next"></div>
                     <div class="swiper-button-prev"></div>
                 </div>
-                <div thumbsSlider="" class="swiper mySwiper">
-                    <div class="swiper-wrapper">
-                        <div class="col-12 col-md-3 swiper-slide">
-                            <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                        <div thumbsSlider="" class="swiper mySwiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-3 swiper-slide">
-                            <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
-                        </div>
-                        <div class="col-12 col-md-3 swiper-slide">
-                            <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
-                        </div>
-                        <div class="col-12 col-md-3 swiper-slide">
-                            <img src="https://dummyimage.com/300x300/d4d4d4/fff" alt="">
-                        </div>
-                    </div>
-                </div>
+                {{-- </div> --}}
+            </div>
+                <div class="col-12 col-md-6">
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos optio repudiandae doloribus itaque vel quo deserunt modi perferendis. Dolor mollitia beatae delectus reprehenderit laborum consequatur facilis reiciendis, facere minus ipsa.</p>
             </div>
             {{-- <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div> --}}
