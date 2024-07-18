bfcbdc6c1c9d5455c0af4cd1412b05ecd3ff5dfe
juriboragine <juri25@outlook.it>
2024-07-18 18:37:11 +0200

    committ

---
+++ lang/es/view.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => 'home',
+    'articles' => 'Annuncios',
+    'contact' => 'Contactos',
+    'login' => 'Login',
+    'register' => 'Register',
+    'account' => 'Area personal',
+    'createArticle' => 'crear anuncio',
+    
+
+];
