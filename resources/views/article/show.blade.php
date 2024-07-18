280da1be52001958aba33a9b321a697794d7d971
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-18 19:12:53 +0200

    fix

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -77,43 +77,9 @@
 </div>
 
 
-{{-- <div class="container">
-    <div class="row">
-        <div class="col-12 d-flex justify-content-center align-items-center mt-5">
-            <div class="card" style="width: 50rem; border:none; ">
-                <div class="col-12 d-flex justify-content-center align-items-center">
-                    <div id="carouselExampleIndicators" class="carousel slide" style="width:800px">
-                        <div class="carousel-indicators">
-                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                            <img src="{{$article->images->isNotEmpty() ? $article->images->first()->getUrl(300, 300) : 'https://dummyimage.com/300x300/d4d4d4/fff' }}" alt="Immagine dell'articolo {{$article->title}}" class="img-fluid rounded-3">
-                          </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                        </button>
-                      </div>
-                </div>
-                <div class="card-body ">
-                  <p class="card-text"><strong>Descrizione:</strong> {{$article->description}}</p>
-                  <p class="card-text"><strong>Categoria:</strong>  {{$article->category->name}}</p>
-                  <p><strong>Autore: </strong>{{Auth::user()->name }}</p>
-                  <a href="#" class="card-link">Card link</a>
-                  <a href="#" class="card-link">Another link</a>
-                </div>
-              </div>
-        </div>
-    </div>
-</div> --}}
+<x-slot:swiper>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</x-slot:swiper>
 
 
 </x-main>
