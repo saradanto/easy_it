e796d60c5f374a9d80edadd98f1577eb43845c0c
Sara D <sara.dantonio@live.it>
2024-07-22 19:51:46 +0200

    added watermark to resizeImage (needed to be fixed)

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Image;
 
 class ResizeImage implements ShouldQueue
@@ -36,6 +37,18 @@ class ResizeImage implements ShouldQueue
         $srcPath = storage_path() . "/app/public/" . $this->path . "/" . $this->fileName;
         $destPath = storage_path() . "/app/public/" . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
-        Image::load($srcPath)->crop($w, $h, CropPosition::Center)->save($destPath);
+        Image::load($srcPath)
+            ->crop($w, $h, CropPosition::Center)
+            ->watermark(
+                base_path('resources/img/easy-it.png'),
+                width: 50,
+                height: 50,
+                paddingX: 5,
+                paddingY: 5,
+                paddingUnit: Unit::Percent,
+
+
+            )
+            ->save($destPath);
     }
 }
