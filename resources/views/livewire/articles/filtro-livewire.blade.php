27f749b3bfbcc9d9b849bcbac98b97f0758c77c8
Stefano Scarpa <scarpastefano@icloud.com>
2024-07-22 19:08:00 +0200

    edit button card color

--- resources/views/livewire/articles/filtro-livewire.blade.php
+++ resources/views/livewire/articles/filtro-livewire.blade.php
@@ -1,9 +1,9 @@
 
 <div class="container mt-3">
-
+    
     <div class="row">
         <div class="col-12 col-md-3 ">
-
+            
             <div class="accordion" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header">
@@ -22,9 +22,9 @@
                     </div>
                 </div>
             </div>
-
-
-
+            
+            
+            
             <div class="accordion mt-5" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header border-bottom-0">
@@ -46,38 +46,40 @@
                     </div>
                 </div>
             </div>
-
-
-
+            
+            
+            
         </div>
-
+        
         <div class="col-12 col-md-9 d-flex flex-wrap justify-content-center">
-
+            
             @foreach ($articles as $article )
-
+            
             <div class="card mx-3 my-3 rounded-3 bg-body-tertiary" style="width: 17rem; min-height: 28rem">
                 {{-- <img src="https://dummyimage.com/300x300/d4d4d4/fff" class="card-img-top img-fluid rounded-top-3" alt="..."> --}}
                 <img src="{{$article->images->isNotEmpty() ? $article->images->first()->getUrl(300, 300) : 'https://dummyimage.com/300x300/d4d4d4/fff' }}" alt="Immagine dell'articolo {{$article->title}}" class="card-img-top img-fluid rounded-top-3">
                 <div class="card-body py-4">
-                  <h5 class="card-title fw-bold h3">{{$article->title}}</h5>
-                  <p class="card-text">{{$article->category->name}}</p>
-                  <div class="d-flex justify-content-between">
-                    <p class="card-text h4 fw-bold">€ {{$article->price}}</p>
-                    <p class="rounded-3"><a class="btn btn-primary" href="{{route('article.show', ['article' => $article])}}">{{__('category.show')}}</a></p>
-                  </div>
+                    <h5 class="card-title fw-bold h3">{{$article->title}}</h5>
+                    <p class="card-text">{{$article->category->name}}</p>
+                    <div class="d-flex justify-content-between">
+                        <p class="card-text h4 fw-bold">€ {{$article->price}}</p>
+                        <p class="rounded-3"><a class="btn btn-primary btn-custom" href="{{route('article.show', ['article' => $article])}}">{{__('category.show')}}</a></p>
+                    </div>
                 </div>
-              </div>
-
+            </div>
+            
             @endforeach
-        </div>
-
-
-        <div class="container">
-            <div class="row">
-                <div class="col-12">{{ $articles->links()}}</div>
+            
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">{{ $articles->links() }}</div>
+                </div>
             </div>
         </div>
-
+        
+        
+        
     </div>
 </div>
 
