49fa5819c6f4587c2913cabbac3349e4a46bd409
gaspare d'antone <dantogas@gmail.com>
2024-07-23 21:09:25 +0200

    fix image

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -57,7 +57,7 @@ class RemoveFaces implements ShouldQueue
             $image = SpatieImage::load($srcPath);
 
             $image->watermark(
-                base_path('resources/img/smile.jpg'),
+                base_path('resources/img/cerchio.png'),
                 AlignPosition::TopLeft,
                 paddingX: $bound[0][0],
                 paddingY: $bound[0][1],
